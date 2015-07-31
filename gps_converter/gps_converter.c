2d51be5b062e532f8140ebf9cbe9d84124080db8
jaanus478 <jns8kgrnd>
2015-07-31 17:47:46 +0300

    added 1 line

--- gps_converter/gps_converter.c
+++ gps_converter/gps_converter.c
@@ -21,6 +21,7 @@ int main(int argc, char *argv[])
 	}
 
 
+
 	char sz[] = "Hello, World!\n";	/* Hover mouse over "sz" while debugging to see its contents */
 	printf("%s", sz);	
 	fflush(stdout); /* <============== Put a breakpoint here */
